a6e70669b781467d37fa94314b89c2838a9ebc4b
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-03-17 14:58:53 +0300

    tests completed

---
+++ src/main/java/org/example/models/DeluxeBurger.java
@@ -0,0 +1,48 @@
+package org.example.models;
+
+public class DeluxeBurger extends Hamburger {
+    private String cips;
+    private String drink;
+
+    public DeluxeBurger() {
+        super("Deluxe Burger", "Double", 19.10, "Double Sandwich");
+        this.cips = "CURVY";
+        this.drink = "COKE";
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("\nDeluxe Burger için yeni malzeme eklenemez");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("\nDeluxe Burger için yeni malzeme eklenemez");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("\nDeluxe Burger için yeni malzeme eklenemez");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("\nDeluxe Burger için yeni malzeme eklenemez");
+    }
+
+    @Override
+    public double itemizeHamburger() {
+        System.out.println("Name: " + getName());
+        System.out.println("Meat: " + getMeat());
+        System.out.println("BreadRollType: " + getBreadRollType());
+        return getPrice();
+    }
+
+    public String getCips() {
+        return cips;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+}
